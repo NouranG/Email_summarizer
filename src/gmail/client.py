02ed64d7f0c7e0822088037084e72011114dc99e
NouranG <nouranlahzy38@gmail.com>
2026-06-09 18:32:16 +0300

    editing client script

--- src/gmail/client.py
+++ src/gmail/client.py
@@ -1,38 +1,39 @@
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-
+from google.oauth2.credentials import Credentials
 from pathlib import Path
 
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
 BASE_DIR = Path(__file__).resolve().parents[2]
-
 AUTH_FILE = BASE_DIR / "auth.json"
-
-SCOPES = [
-    "https://www.googleapis.com/auth/gmail.readonly"
-]
 
 
 def get_credentials():
-
-        flow = InstalledAppFlow.from_client_secrets_file(
-            AUTH_FILE,
-            SCOPES
-        )
-        return flow.run_local_server(port=0)
+    flow = InstalledAppFlow.from_client_secrets_file(
+        AUTH_FILE,
+        SCOPES
+    )
+    return flow.run_local_server(port=0)
 
 
 class GmailClient:
-    def __init__(self, credentials: Credentials):
-        self.service = build('gmail', 'v1', credentials=credentials)
 
-    def list_messages(self, user_id='me', max_results=10):
-        results = self.service.users().messages().list(userId=user_id, maxResults=max_results).execute()
-        return results.get('messages', [])
-    def get_unread_messages(self):
-        response = self.service.users().messages().list(
+    def __init__(self, credentials: Credentials):
+        self.service = build("gmail", "v1", credentials=credentials)
+
+    def list_unread(self, max_results=10):
+        res = self.service.users().messages().list(
             userId="me",
-            q="is:unread"
+            q="is:unread",
+            maxResults=max_results
         ).execute()
 
-        return response.get("messages", [])
+        return res.get("messages", [])
+
+    def get_message_raw(self, message_id):
+        return self.service.users().messages().get(
+            userId="me",
+            id=message_id,
+            format="raw"
+        ).execute()
